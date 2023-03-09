3588ff07932e69a4effba73a47695551d2eeda85
dinako81 <dina.koliata@gmail.com>
2023-03-09 22:53:41 +0200

    update

--- src/ciklai.php
+++ src/ciklai.php
@@ -10,32 +10,47 @@
     }
 
     echo '<br>';
-    echo '***********************';
+    echo '***********1a************';
     echo '<br>';
 
-    '<div className="aaa">';
+    ?>
+
+    <div style="word-wrap: break-word">
+
+    <?php
     for ($i=0; $i<400; $i++){
         echo '*';
     }
-    '</div>'; 
-   
+   ?>
+   </div>
+
+   <?php
     echo '<br>';
-    echo '***********************';
+    echo '***********1b************';
     echo '<br>';
 
-    $string='<div>';
     for ($i=0; $i<400; $i++){
-        echo '*';
+        if (($i+1) % 50 == 0) {
+            echo '<br>';
+      }
+
+    echo '<br>';
+    echo '************2***********';
+    echo '<br>';
+// 2.Sugeneruokit 300 atsitiktinių skaičių nuo 0 iki 300, atspausdinkite juos atskirtus tarpais ir suskaičiuokite kiek tarp jų yra didesnių už 150.  Skaičiai didesni nei 275 turi būti raudonos spalvos.
+
+$count = 0;
+for ($i=0; $i<300; $i++) {
+    $a = rand(0, 300). " ";
+    if ($a>275){
+        echo "<span style='color:red;'>$a</span> ";
+    } else echo $a;
+    
+    if ($a>150){
+        $count++;
     }
-    '</div>'; 
-    $newstring = wordwrap($string, 50, "<br />\n");
- ///neveikia
-
-
-    echo '<br>';
-    echo '***********************';
-    echo '<br>';
-
-
+}
+echo "<br>";
+echo "Didesnių už 150 skaičių kiekis: $count";
     ?>
 
