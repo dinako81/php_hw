16596cb791cc4a79a133e0dc688a7c5b2b0c3652
dinako81 <dina.koliata@gmail.com>
2023-04-05 14:59:10 +0300

    delete, insert, update sql

--- 025/index.php
+++ 025/index.php
@@ -26,15 +26,83 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $sql = "
         INSERT INTO trees
         (name, heigth, type)
-        VALUES ('".$_POST['name']."', ".$_POST['heigth'].", ".$_POST['type'].")
+        VALUES (?, ?, ?)
         ";
 
-        $pdo->query($sql);
-        header('Location: http://localhost/ciupakabros/php_hw/025/');
-        die;
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->execute([
+            $_POST['name'],
+            $_POST['heigth'],
+            $_POST['type']
+        ]);
+            header('Location: http://localhost/ciupakabros/php_hw/025/');
+            die;
 
     }
+        if ($_POST['_method'] == 'delete') {
+
+            // DELETE FROM table_name WHERE condition;
+
+            $sql = "
+            DELETE 
+            FROM trees
+            WHERE id = ?
+            ";
+
+
+            $stmt = $pdo->prepare($sql);
+
+        $stmt->execute([$_POST['id']]);
+
+        header('Location: http://localhost/ciupakabros/php_hw/025/');
+        die;
+        }
+
+        if ($_POST['_method'] == 'put') {
+
+            // UPDATE table_name
+            // SET column1 = value1, column2 = value2, ...
+            // WHERE condition;
+            $sql = "
+            UPDATE trees
+            SET name = ?, heigth = ?, type = ?
+            WHERE id = ?
+        ";
+    
+        $stmt = $pdo->prepare($sql);
+    
+        $stmt->execute([
+            $_POST['name'],
+            $_POST['heigth'],
+            $_POST['type'],
+            $_POST['id']
+        ]);
+        header('Location: http://localhost/ciupakabros/php_hw/025/');
+        die;
+        }    
+
+
+    if ($_POST['_method'] == 'up') {
+        // UPDATE table_name
+        // SET column1 = value1, column2 = value2, ...
+        // WHERE condition;
+
+    $sql = "
+        UPDATE trees
+        SET heigth = heigth + 1
+        WHERE 1
+    ";
+
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->execute([]);
+    header('Location: http://localhost/ciupakabros/php_hw/025/');
+    die;
+    }
 }
+
+
 
 // SELECT column1, column2, ...
 // FROM table_name;
@@ -64,6 +132,15 @@ ORDER by name
 $stmt = $pdo->query($sql);
 $trees = $stmt->fetchall();
 
+$sql = "
+    SELECT sum(heigth) AS heigths
+    FROM trees
+";
+
+$stmt = $pdo->query($sql);
+
+$sum = $stmt->fetch();
+
 ?>
 
 <!DOCTYPE html>
@@ -78,18 +155,29 @@ $trees = $stmt->fetchall();
 </head>
 
 <body>
-    <div class="bin">
-        <ul>
-            <?php foreach ($trees as $tree): ?>
-            <li>
-                <div class="id"> <?= $tree ['id'] ?> </div>
-                <div class="name"> <?= $tree ['name'] ?> </div>
-                <div class="h"> <?= $tree ['heigth'] ?> </div>
-                <div class="t"> <?=  ['Lapuotis', 'Spygliuotis', 'Palmė'][$tree['type']-1] ?> </div>
-            </li>
-            <?php endforeach ?>
-        </ul>
 
+    <ul>
+        <?php foreach($trees as $tree) : ?>
+        <li>
+            <div class="id"><?= $tree['id'] ?></div>
+            <div class="name"><?= $tree['name'] ?></div>
+            <div class="h"><?= $tree['heigth'] ?> m</div>
+            <div class="t"><?= ['Lapuotis', 'Spygliuotis', 'Palmė'][$tree['type'] - 1] ?></div>
+        </li>
+        <?php endforeach ?>
+        <li>
+            <div class="id"></div>
+            <div class="name">Bendras aukštis</div>
+            <div class="h"><?= $sum['heigths'] ?> m</div>
+            <div class="t"></div>
+        </li>
+    </ul>
+    <form method="post">
+        <input type="hidden" name="_method" value="up">
+        <div><button type="submit">Lietus</div>
+    </form>
+
+    <div class="bin">
         <form method="post">
             <div>
                 <label>Vardas</label>
@@ -104,14 +192,51 @@ $trees = $stmt->fetchall();
                 <select name="type">
                     <option value="1">Lapuotis</option>
                     <option value="2">Spygliuotis</option>
-                    <option value="3">palmė</option>
+                    <option value="3">Palmė</option>
                 </select>
             </div>
             <input type="hidden" name="_method" value="post">
             <div><button type="submit">Sodinti</div>
-
         </form>
+
+        <form method="post">
+            <div>
+                <label>ID</label>
+                <input type="text" name="id">
+            </div>
+            <div>
+                <label>Vardas</label>
+                <input type="text" name="name">
+            </div>
+            <div>
+                <label>Aukštis</label>
+                <input type="range" name="heigth">
+            </div>
+            <div>
+                <label>Tipas</label>
+                <select name="type">
+                    <option value="1">Lapuotis</option>
+                    <option value="2">Spygliuotis</option>
+                    <option value="3">Palmė</option>
+                </select>
+            </div>
+            <input type="hidden" name="_method" value="put">
+            <div><button type="submit">Persodinti</div>
+        </form>
+
+        <form method="post">
+            <div>
+                <label>ID</label>
+                <input type="text" name="id">
+            </div>
+
+            <input type="hidden" name="_method" value="delete">
+            <div><button type="submit">Pjauti</div>
+        </form>
+
     </div>
+
+
 </body>
 
 </html>
